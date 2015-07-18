fe6bff02239647f6e549975455216d45f3ef8c36
Vee Satayamas <vsatayamas@gmail.com>
2015-07-17 23:53:12 -0400

    modify in order to build on Fedora 22 without Dropbox

--- armhf/build-cargo.sh
+++ armhf/build-cargo.sh
@@ -16,7 +16,7 @@ set -e
 : ${DROPBOX:=dropbox_uploader.sh}
 : ${MAX_NUMBER_OF_NIGHTLIES:=10}
 : ${NIGHTLY_DIR:=~/nightly}
-: ${SRC_DIR:=~/src}
+: ${SRC_DIR:=~/cargo}
 
 CARGO_NIGHTLY_DIR=$NIGHTLY_DIR/cargo
 RUST_NIGHTLY_DIR=$NIGHTLY_DIR/rust
@@ -32,7 +32,7 @@ git checkout $1
 git submodule update
 
 # apply patch to link statically against libssl
-git apply /ruststrap/armhf/static-ssl.patch
+git apply $HOME/ruststrap/armhf/static-ssl.patch
 
 # get information about HEAD
 HEAD_HASH=$(git rev-parse --short HEAD)
@@ -40,96 +40,41 @@ HEAD_DATE=$(TZ=UTC date -d @$(git show -s --format=%ct HEAD) +'%Y-%m-%d')
 TARBALL=cargo-$HEAD_DATE-$HEAD_HASH-arm-unknown-linux-gnueabihf
 LOGFILE=cargo-$HEAD_DATE-$HEAD_HASH.test.output.txt
 
-# check if we have build this exact version of cargo
-if [ ! -z "$($DROPBOX list | grep $HEAD_DATE-$HEAD_HASH)" ]; then
-  exit 0
-fi
+## test rust and cargo nightlies
+rustc -V
 
-# XXX It's possible that cargo won't build with the latest cargo nightly, so
-# I should try all the available nightlies. However, I haven't seen that
-# happen in practice yet, so I'll just try the latest cargo nightly for now
-# install cargo nightly
-cd $CARGO_NIGHTLY_DIR
-rm -rf *
-CARGO_NIGHTLY=$($DROPBOX list . | grep cargo- | grep tar | tail -n 1 | tr -s ' ' | cut -d ' ' -f 4)
-$DROPBOX -p download $CARGO_NIGHTLY
-tar xzf $CARGO_NIGHTLY
-rm $CARGO_NIGHTLY
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$RUST_NIGHTLY_DIR/lib:$CARGO_NIGHTLY_DIR/lib
-
-# cargo doesn't always build with my latest rust nightly, so try all the
-# nightlies available.
-# FIXME the right way to do this would use the date in the src/rustversion.txt
-# file
-for RUST_NIGHTLY in $($DROPBOX list . | grep rust- | grep tar | tr -s ' ' | cut -d ' ' -f 4 | sort -r); do
-  ## install nigthly rust
-  cd $RUST_NIGHTLY_DIR
-  rm -rf *
-  $DROPBOX -p download $RUST_NIGHTLY
-  tar xzf $RUST_NIGHTLY
-  rm $RUST_NIGHTLY
-
-  ## test rust and cargo nightlies
-  $RUST_NIGHTLY_DIR/bin/rustc -V
-  PATH=$PATH:$RUST_NIGHTLY_DIR/bin $CARGO_NIGHTLY_DIR/bin/cargo -V
-
-  ## build it, if compilation fails try the next nightly
-  cd $SRC_DIR
-  ./configure \
+## build it, if compilation fails try the next nightly
+cd $SRC_DIR
+./configure \
     --disable-verify-install \
     --enable-nightly \
     --enable-optimize \
-    --local-cargo=$CARGO_NIGHTLY_DIR/bin/cargo \
-    --local-rust-root=$RUST_NIGHTLY_DIR \
+    --local-cargo=$HOME/bin/cargo \
+    --local-rust-root=$HOME \
     --prefix=/
   make clean
-  make || continue
+  make || exit 1
 
-  ## package
-  rm -rf $DIST_DIR/*
-  DESTDIR=$DIST_DIR make install
-  cd $DIST_DIR
-  # smoke test the produced cargo nightly
-  PATH=$PATH:$RUST_NIGHTLY_DIR/bin LD_LIBRARY_PATH=$LD_LIBRARY_PATH:lib bin/cargo -V
-  tar czf ~/$TARBALL .
-  cd ~
-  TARBALL_HASH=$(sha1sum $TARBALL | tr -s ' ' | cut -d ' ' -f 1)
-  mv $TARBALL $TARBALL-$TARBALL_HASH.tar.gz
-  TARBALL=$TARBALL-$TARBALL_HASH.tar.gz
+## package
+rm -rf $DIST_DIR/*
+DESTDIR=$DIST_DIR make install
+cd $DIST_DIR
 
-  # ship it
-  if [ -z $DONTSHIP ]; then
-    $DROPBOX -p upload $TARBALL .
-  fi
-  rm $TARBALL
+# smoke test the produced cargo nightly
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:lib bin/cargo -V
 
-  # delete older nightlies
-  NUMBER_OF_NIGHTLIES=$($DROPBOX list . | grep cargo- | grep tar | wc -l)
-  for i in $(seq `expr $MAX_NUMBER_OF_NIGHTLIES + 1` $NUMBER_OF_NIGHTLIES); do
-    OLDEST_NIGHTLY=$($DROPBOX list . | grep cargo- | grep tar | head -n 1 | tr -s ' ' | cut -d ' ' -f 4)
-    $DROPBOX delete $OLDEST_NIGHTLY
-    OLDEST_TEST_OUTPUT=$(echo $OLDEST_NIGHTLY | cut -d '-' -f 1-5).test.output.txt
-    $DROPBOX delete $OLDEST_TEST_OUTPUT || true
-  done
+tar czf ~/$TARBALL .
 
-  # run tests
-  if [ -z $DONTTEST ]; then
+cd ~
+TARBALL_HASH=$(sha1sum $TARBALL | tr -s ' ' | cut -d ' ' -f 1)
+mv $TARBALL $TARBALL-$TARBALL_HASH.tar.gz
+TARBALL=$TARBALL-$TARBALL_HASH.tar.gz
+
+# run tests
+if [ -z $DONTTEST ]; then
     cd $SRC_DIR
     uname -a > $LOGFILE
-    $RUST_NIGHTLY_DIR/bin/rustc -V >> $LOGFILE
+    rustc -V >> $LOGFILE
     echo >> $LOGFILE
     RUST_TEST_THREADS=1 make test -k >>$LOGFILE 2>&1 || true
-    $DROPBOX -p upload $LOGFILE .
-    rm $LOGFILE
-  fi
-
-  # cleanup
-  rm -rf $CARGO_NIGHTLY_DIR/*
-  rm -rf $DIST_DIR/*
-  rm -rf $RUST_NIGHTLY_DIR/*
-
-  exit 0
-done
-
-exit 1
+fi
--- armhf/build-rust.sh
+++ armhf/build-rust.sh
@@ -14,10 +14,8 @@ set -e
 
 : ${CHANNEL:=nightly}
 : ${DIST_DIR:=~/dist}
-: ${DROPBOX:=dropbox_uploader.sh}
+: ${SRC_DIR:=~/rust}
 : ${MAX_NUMBER_OF_NIGHTLIES:=10}
-: ${SNAP_DIR:=~/snap}
-: ${SRC_DIR:=~/src}
 
 case $CHANNEL in
     beta | stable ) CHANNEL=--release-channel=$CHANNEL;;
@@ -34,23 +32,7 @@ git pull
 git checkout $1
 git submodule update
 
-# Get the hash and date of the latest snaphot
-SNAP_HASH=$(head -n 1 src/snapshots.txt | tr -s ' ' | cut -d ' ' -f 3)
-
-# Check if the snapshot is available
-SNAP_TARBALL=$($DROPBOX list snapshots | grep $SNAP_HASH | grep tar)
-if [ -z "$SNAP_TARBALL" ]; then
-  exit 1
-fi
-SNAP_TARBALL=$(echo $SNAP_TARBALL | tr -s ' ' | cut -d ' ' -f 3)
-
-# setup snapshot
-cd $SNAP_DIR
-rm -rf *
-$DROPBOX -p download snapshots/$SNAP_TARBALL
-tar xjf $SNAP_TARBALL --strip-components=1
-rm $SNAP_TARBALL
-bin/rustc -V
+rustc -V
 
 # Get information about HEAD
 cd $SRC_DIR
@@ -66,14 +48,14 @@ cd build
   --disable-valgrind \
   --enable-ccache \
   --enable-local-rust \
-  --enable-llvm-static-stdcpp \
   --local-rust-root=$SNAP_DIR \
   --prefix=/ \
+  --enable-llvm-static-stdcpp \
   --build=arm-unknown-linux-gnueabihf \
   --host=arm-unknown-linux-gnueabihf \
   --target=arm-unknown-linux-gnueabihf
 make clean
-make
+make -j$(nproc)
 
 # package
 rm -rf $DIST_DIR/*
@@ -85,31 +67,10 @@ TARBALL_HASH=$(sha1sum $TARBALL | tr -s ' ' | cut -d ' ' -f 1)
 mv $TARBALL $TARBALL-$TARBALL_HASH.tar.gz
 TARBALL=$TARBALL-$TARBALL_HASH.tar.gz
 
-# ship it
-if [ -z $DONTSHIP ]; then
-  $DROPBOX -p upload $TARBALL .
-fi
-rm $TARBALL
-
-# delete older nightlies
-NUMBER_OF_NIGHTLIES=$($DROPBOX list . | grep rust- | grep tar | wc -l)
-for i in $(seq `expr $MAX_NUMBER_OF_NIGHTLIES + 1` $NUMBER_OF_NIGHTLIES); do
-  OLDEST_NIGHTLY=$($DROPBOX list . | grep rust- | grep tar | head -n 1 | tr -s ' ' | cut -d ' ' -f 4)
-  $DROPBOX delete $OLDEST_NIGHTLY
-  OLDEST_TEST_OUTPUT=$(echo $OLDEST_NIGHTLY | cut -d '-' -f 1-5).test.output.txt
-  $DROPBOX delete $OLDEST_TEST_OUTPUT || true
-done
-
 # run tests
 if [ -z $DONTTEST ]; then
   cd $SRC_DIR/build
   uname -a > $LOGFILE
   echo >> $LOGFILE
   RUST_TEST_THREADS=1 timeout 7200 make check -k >>$LOGFILE 2>&1 || true
-  $DROPBOX -p upload $LOGFILE .
-  rm $LOGFILE
 fi
-
-# cleanup
-rm -rf $DIST_DIR/*
-rm -rf $SNAP_DIR/*
